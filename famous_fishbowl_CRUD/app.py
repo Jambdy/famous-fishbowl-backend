596b3472219cc7ce0b794e0491d85bbd8dd3c57f
James Abdy <jambdy@gmail.com>
2023-09-24 21:26:50 -0400

    add github actions

--- famous_fishbowl_CRUD/app.py
+++ famous_fishbowl_CRUD/app.py
@@ -17,20 +17,20 @@ def lambda_handler(event, _):
 
     try:
         if event['httpMethod'] == 'DELETE':
-            id = event['pathParameters']['id']
+            id_str = event['pathParameters']['id']
             dynamodb.delete_item(
                 TableName=table_name,
                 Key={
-                    'ID': {'S': id}
+                    'id': {'S': id_str}
                 }
             )
-            body = f'Deleted item {id}'
+            body = f'Deleted item {id_str}'
         elif event['httpMethod'] == 'GET':
-            id = event['pathParameters']['id']
+            id_str = event['pathParameters']['id']
             response = dynamodb.get_item(
                 TableName=table_name,
                 Key={
-                    'ID': {'S': id}
+                    'id': {'S': id_str}
                 }
             )
             if 'Item' in response:
@@ -38,12 +38,12 @@ def lambda_handler(event, _):
                 body = json.dumps(item)
             else:
                 status_code = 404
-                body = f'Game {id} not found'
+                body = f'Game {id_str} not found'
         elif event['httpMethod'] == 'PUT':
             request_json = json.loads(event['body'])
             dynamodb.put_item(
                 TableName=table_name,
-                Item = {
+                Item={
                     'id': request_json['id'],
                     'state': request_json['state'],
                     'creationTime': request_json['creationTime'],
