e306783b17f18be9db73d068b4dd9726edc3543d
James Abdy <jambdy@gmail.com>
2023-09-25 23:35:09 -0400

    update dynamo update

--- famous_fishbowl_CRUD/app.py
+++ famous_fishbowl_CRUD/app.py
@@ -1,10 +1,10 @@
-import json
+import simplejson as json
 import boto3
 
 
 def lambda_handler(event, _):
-    table_name = 'famous-fishbowl'
-    dynamodb = boto3.client('dynamodb')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('famous-fishbowl')
     headers = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Headers': '*',
@@ -18,48 +18,46 @@ def lambda_handler(event, _):
     try:
         if event['httpMethod'] == 'DELETE':
             id_str = event['pathParameters']['id']
-            dynamodb.delete_item(
-                TableName=table_name,
+            table.delete_item(
                 Key={
-                    'id': {'S': id_str}
+                    'id': id_str
                 }
             )
             body = f'Deleted item {id_str}'
         elif event['httpMethod'] == 'GET':
             id_str = event['pathParameters']['id']
-            response = dynamodb.get_item(
-                TableName=table_name,
+            response = table.get_item(
                 Key={
-                    'id': {'S': id_str}
+                    'id': id_str
                 }
             )
             if 'Item' in response:
                 item = response['Item']
-                body = json.dumps(item)
+                body = json.dumps(item, use_decimal=True)
             else:
                 status_code = 404
                 body = f'Game {id_str} not found'
         elif event['httpMethod'] == 'PUT':
             request_json = json.loads(event['body'])
-            dynamodb.put_item(
-                TableName=table_name,
+            table.put_item(
                 Item={
-                    'id': {'S': request_json['id']},
-                    'state': {'S': request_json['state']},
-                    'creationTime': {'S': request_json['creationTime']},
-                    'completionTime': {'S': request_json['completionTime']},
-                    'categories': {'SS': request_json['categories']},
-                    'names': {'M': request_json['names']},
-                    'round': {'N': request_json['round']},
-                    'curTeam': {'N': request_json['curTeam']},
-                    'teamScores': {'NS': request_json['teamScores']},
-                    'timeRemaining': {'N': request_json['timeRemaining']},
-                    'turnResumeTime': {'S': request_json['turnResumeTime']}
+                    'id': request_json['id'],
+                    'state': request_json['state'],
+                    'creationTime': request_json['creationTime'],
+                    'completionTime': request_json['completionTime'],
+                    'categories': request_json['categories'],
+                    'names': request_json['names'],
+                    'round': request_json['round'],
+                    'curTeam': request_json['curTeam'],
+                    'teamScores': request_json['teamScores'],
+                    'timeRemaining': request_json['timeRemaining'],
+                    'turnResumeTime': request_json['turnResumeTime']
                 }
             )
             body = f'Put Game {request_json["id"]}'
     except Exception as err:
         body = f"Unexpected {err}, {type(err)}"
+        print(body)
         status_code = 400
 
     return {
