ba2b63b59acf8e7c59635040a93e67142863b3a8
James Abdy <jambdy@gmail.com>
2023-10-22 19:11:30 -0400

    add names lambda

---
+++ famous_fishbowl_CRUD/game_add_names.py
@@ -0,0 +1,46 @@
+import simplejson as json
+import boto3
+
+
+def lambda_handler(event, _):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('famous-fishbowl')
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,PUT',
+        'Access-Control-Allow-Credentials': True
+    }
+    body = ''
+    status_code = 200
+
+    try:
+        id_str = event['pathParameters']['id']
+        request_json = json.loads(event['body'])
+
+        update_string = 'SET names = list_append(names, :names)'
+        expression_values = {
+            ':names': request_json['names']
+        }
+
+        table.update_item(
+            Key={
+                'pk': 'gameInstance',
+                'sk': id_str
+            },
+            UpdateExpression=update_string,
+            ExpressionAttributeValues=expression_values
+        )
+        body = f'Added Names to Game {id_str}'
+    except Exception as err:
+        body = f"Unexpected {err}, {type(err)}"
+        print(body)
+        status_code = 400
+
+    return {
+        'statusCode': status_code,
+        'body': body,
+        'headers': headers
+    }
+
